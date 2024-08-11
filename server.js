28c028edbf65a51217515e089d261992235089c1
Cesarsilva000 <101932159+Cesarsilva000@users.noreply.github.com>
2024-08-11 14:36:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-server.js// Listen on a specific host via the HOST environment variable
+// Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
-// Grab the blacklist from the command-line so that we can update the blacklist without deploying
-// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
-// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
-// use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+// Function to parse environment variable lists
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -16,13 +11,20 @@ function parseEnvList(env) {
   return env.split(',');
 }
 
+// Grab the blacklist from the command-line so that we can update the blacklist without deploying again.
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
 
+// Import cors-anywhere library
 var cors_proxy = require('./lib/cors-anywhere');
+
+// Create the CORS proxy server
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: [], // Allow all origins
+  originWhitelist: originWhitelist, // Allow all origins if the whitelist is empty
   // Comment out the requireHeader option to not require any headers
   // requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
